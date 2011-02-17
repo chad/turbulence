62ceae61127b8f5a4f69699d3854dd55ce91d9fe
Chad Fowler <chad@chadfowler.com>
2011-02-17 11:27:20 -0700

    print churn and flog

--- lib/chuggle.rb
+++ lib/chuggle.rb
@@ -3,34 +3,38 @@ require 'stringio'
 
 class Chuggle
   attr_reader :dir
+  attr_reader :metrics
   def initialize(dir)
     @dir = dir
+    @metrics = {}
     Dir.chdir(dir) do
       @ruby_files = Dir["**/*\.rb"]
+      churn
+      flog
     end
   end
 
   def churn
-    Dir.chdir(dir) do
-      raw = `git log --all -M -C --name-only | grep -E '^(app|lib)/' | sort | uniq -c | sort`.split(/\n/).map(&:split)
-      raw.select do |count, filename|
-        filename =~ /rb$/
-      end.map do |count, filename|
-        [Integer(count), filename]
-      end
+    raw = `git log --all -M -C --name-only| sort | uniq -c | sort`.split(/\n/).map(&:split)
+    raw.select do |count, filename|
+      filename =~ /\.rb$/
+    end.each do |count, filename|
+      metrics_for(filename)[:churn] = Integer(count)
     end
   end
 
   def flog
-    Dir.chdir(dir) do
-      flogger = Flog.new
-      @ruby_files.map do |filename|
-        p filename
-        flogger.flog filename
-        report = StringIO.new
-        flogger.report(report)
-        report.to_s.scan(/^.+average$/).first
-      end 
-    end
+    flogger = Flog.new
+    @ruby_files.each do |filename|
+      flogger.flog filename
+      report = StringIO.new
+      flogger.report(report)
+      score = Float(report.string.scan(/^\s+([^:]+).*average$/).flatten.first)
+      metrics_for(filename)[:flog] = score
+    end 
+  end
+  
+  def metrics_for(filename)
+    @metrics[filename] ||= {}
   end
 end
