7adc5b796f66153be537f2f0034195f13e925d79
coreyhaines <coreyhaines@gmail.com>
2011-02-25 16:39:23 -0700

    Add filename to graph tooltip

--- lib/chuggle/scatter_plot_generator.rb
+++ lib/chuggle/scatter_plot_generator.rb
@@ -2,8 +2,8 @@ require 'json'
 class Chuggle
   class ScatterPlotGenerator
     def self.from(metrics_hash)
-      data_in_json_format = metrics_hash.map do |_, metrics|
-        [metrics[:churn], metrics[:complexity]]
+      data_in_json_format = metrics_hash.map do |filename, metrics|
+        {:filename => filename, :x => metrics[:churn], :y => metrics[:complexity]}
       end.to_json
       "var chuggleGraphData = #{data_in_json_format};"
     end
