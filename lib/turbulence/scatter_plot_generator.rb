d5e5d5e57dcee5adf83ecfcc129e7b494b0dacac
coreyhaines <coreyhaines@coreyhainess-MacBook-Air.local>
2011-02-28 07:56:02 -0700

    Formatting of chart

--- lib/turbulence/scatter_plot_generator.rb
+++ lib/turbulence/scatter_plot_generator.rb
@@ -2,14 +2,6 @@ require 'json'
 class Turbulence
   class ScatterPlotGenerator
     def self.from(metrics_hash)
-
-      data_in_json_format = metrics_hash.map do |filename, metrics|
-        {:filename => filename, :x => metrics[:churn], :y => metrics[:complexity]}
-      end.reject do |metrics|
-          metrics[:x].nil? || metrics[:y].nil?
-      end.to_json
-      series = ["var turbulenceGraphData = #{data_in_json_format};"]
-
       grouped_by_directory = metrics_hash.group_by do |filename, _|
         directories = File.dirname(filename).split("/")
         directories[0..1].join("/")
@@ -20,14 +12,12 @@ class Turbulence
         data_in_json_format = metrics_hash.map do |filename, metrics|
           {:filename => filename, :x => metrics[:churn], :y => metrics[:complexity]}
         end.reject do |metrics|
-            metrics[:x].nil? || metrics[:y].nil?
+          metrics[:x].nil? || metrics[:y].nil?
         end
         directory_series[directory] = data_in_json_format
       end
 
-      series << "var directorySeries = #{directory_series.to_json};"
-
-      series.join("\n")
+      "var directorySeries = #{directory_series.to_json};"
     end
   end
 end
