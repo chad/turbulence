a5a95a09611935cff1128a0afb4a2adc833e6708
Chad Fowler <chad@chadfowler.com>
2011-03-01 08:18:40 -0700

    moved mangler inside namespace

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 Hopefully-meaningful Metrics
 ============================
 
-A quick hack based on Michael Feathers' recent work in project churn and complexity:
+Based on Michael Feathers' recent work in project churn and complexity:
 http://www.stickyminds.com/sitewide.asp?Function=edetail&ObjectType=COL&ObjectId=16679&tth=DYN&tt=siteemail&iDyn=2
 
 
--- lib/turbulence/scatter_plot_generator.rb
+++ lib/turbulence/scatter_plot_generator.rb
@@ -1,21 +1,21 @@
 require 'json'
 
-class FileNameMangler
-  def initialize
-    @current_id = 0
-    @segment_map = { "app" => "app", "controllers" => "controllers", "helpers" => "helpers", "lib" => "lib" }
-  end
-
-  def transform(segment)
-    @segment_map[segment] ||= (@current_id += 1)
-  end
-
-  def mangle_name(filename)
-    filename.split('/').map {|seg|transform(seg)}.join('/') + ".rb"
-  end
-end
-
 class Turbulence
+  class FileNameMangler
+    def initialize
+      @current_id = 0
+      @segment_map = { "app" => "app", "controllers" => "controllers", "helpers" => "helpers", "lib" => "lib" }
+    end
+
+    def transform(segment)
+      @segment_map[segment] ||= (@current_id += 1)
+    end
+
+    def mangle_name(filename)
+      filename.split('/').map {|seg|transform(seg)}.join('/') + ".rb"
+    end
+  end
+
   class ScatterPlotGenerator
     def self.from(metrics_hash)
       new(metrics_hash)
