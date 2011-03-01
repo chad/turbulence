c63d21eeb122c93aa85a5bc4cb384fe97657eb41
coreyhaines <coreyhaines@gmail.com>
2011-02-28 17:53:19 -0700

    General cleanup of CommandLineInterface

--- lib/turbulence/command_line_interface.rb
+++ lib/turbulence/command_line_interface.rb
@@ -3,21 +3,18 @@ require 'launchy'
 
 class Turbulence
   class CommandLineInterface
-    TURBULENCE_PATH = File.join(File.absolute_path(File.dirname(__FILE__)), "..", "..")
+    TURBULENCE_TEMPLATE_PATH = File.join(File.absolute_path(File.dirname(__FILE__)), "..", "..", "template")
+    TEMPLATE_FILES = ['turbulence.html', 'highcharts.js', 'jquery.min.js'].map { |filename|
+      File.join(TURBULENCE_TEMPLATE_PATH, filename)
+    }
 
     attr_reader :directory
     def initialize(directory)
       @directory = directory
     end
 
-    def path_to_template(filename)
-      File.join(TURBULENCE_PATH, "template", filename)
-    end
-
     def copy_templates_into(directory)
-      ['turbulence.html', 'highcharts.js', 'jquery.min.js'].each do |filename|
-        FileUtils.cp path_to_template(filename), directory
-      end
+      FileUtils.cp TEMPLATE_FILES, directory
     end
 
     def generate_bundle
