f2a9ed33deead27a1de736c8eb6779e94777470f
kitofr <kitofr@gmail.com>
2011-03-02 17:31:07 +0100

    Failing test. Probably need to stub/mock this to not rely on standing in a git repo

--- spec/turbulence/checks_environoment_spec.rb
+++ spec/turbulence/checks_environoment_spec.rb
@@ -3,4 +3,8 @@ require 'turbulence'
 
 describe Turbulence::ChecksEnvironment do
 
+  it "should determine if the current directory is a git repository" do
+    Turbulence::ChecksEnvironment.git_repo?(Dir.pwd).should be_true
+
+  end    
 end
