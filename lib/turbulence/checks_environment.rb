c374bd6eb46e0a08f64f66dd56bd405269182015
kitofr <kitofr@gmail.com>
2011-03-03 09:58:02 +0100

    Added in platform checking

--- lib/turbulence/checks_environment.rb
+++ lib/turbulence/checks_environment.rb
@@ -2,9 +2,19 @@ require 'open3'
 class Turbulence
   class ChecksEnvironment
     class << self
-      def git_repo?(directory)
-        Open3.popen3("git status") do |_, _, err, _|
-          return !(err.read =~ /Not a git repository/)  
+      if (RUBY_PLATFORM =~ /mswin|mingw/)
+        def git_repo?(directory)
+          Dir.chdir directory do
+            !(`git status` =~ /Not a git repository/)
+          end
+        end
+      else
+        def git_repo?(directory)
+          Dir.chdir direcotry do
+            Open3.popen3("git status") do |_, _, err, _|
+              return !(err.read =~ /Not a git repository/)  
+            end
+          end
         end
       end
     end
