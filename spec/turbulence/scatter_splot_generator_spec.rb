bdf38bd9ba9c935643094305802d7b15b20f2f88
Chad Fowler <chad@chadfowler.com>
2011-03-02 18:28:48 -0700

    Thanks Dave Hoover for more specs and cleaner code

--- spec/turbulence/scatter_splot_generator_spec.rb
+++ spec/turbulence/scatter_splot_generator_spec.rb
@@ -1,6 +1,29 @@
 require 'turbulence'
 
 describe Turbulence::ScatterPlotGenerator do
+  context "with both Metrics" do
+    it "generates JavaScript" do
+      generator = Turbulence::ScatterPlotGenerator.new(
+        "foo.rb" => {
+          Turbulence::Calculators::Churn => 1,
+          Turbulence::Calculators::Complexity => 2
+        }
+      )
+      generator.to_js.should == 'var directorySeries = {".":[{"filename":"foo.rb","x":1,"y":2}]};'
+    end  
+  end
+
+  context "with a missing Metric" do
+    it "generates JavaScript" do
+      generator = Turbulence::ScatterPlotGenerator.new(
+        "foo.rb" => {
+          Turbulence::Calculators::Churn => 1
+        }
+      )
+      generator.to_js.should == 'var directorySeries = {};'
+    end  
+  end
+
   describe Turbulence::FileNameMangler do
     subject { Turbulence::FileNameMangler.new }
     it "anonymizes a string" do
