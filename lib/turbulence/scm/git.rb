abbc8dfe6b5f0ddf8e9576a3916f84a599d5daae
Henrik Wist <henrik.wist@titanias.org>
2011-03-08 17:02:37 +0100

    Fix merge error

--- lib/turbulence/scm/git.rb
+++ lib/turbulence/scm/git.rb
@@ -1,25 +1,3 @@
-<<<<<<< HEAD
-require 'open3'
-class Turbulence
-  module Scm
-    class Git 
-      class << self
-        def log_command(commit_range = "")
-          `git log --all -M -C --numstat --format="%n" #{commit_range}`
-        end
-
-        def is_repo?(directory)
-          FileUtils.cd(directory) {
-            Open3.popen3("git status") do |_, _, err, _|
-            return !(err.read =~ /Not a git repository/)  
-            end
-          }
-        end
-      end
-    end
-  end
-end
-=======
 class Turbulence
   module Scm
     class Git
@@ -37,4 +15,3 @@ class Turbulence
     end
   end
 end
->>>>>>> 007db25d47381e9fbac9634c35752c4d414a5151
