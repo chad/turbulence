5adcf71362a36d2610774262008011a0bbf3e719
Sam Livingston-Gray <geeksam@gmail.com>
2014-03-05 11:43:03 -0800

    Delegate attrs from CLI to config instead of storing as ivars

--- lib/turbulence/command_line_interface.rb
+++ lib/turbulence/command_line_interface.rb
@@ -1,6 +1,8 @@
 require 'fileutils'
 require 'launchy'
 require 'optparse'
+require 'forwardable'
+
 require 'turbulence/configuration'
 require 'turbulence/scm/git'
 require 'turbulence/scm/perforce'
@@ -15,18 +17,20 @@ class Turbulence
       File.join(TURBULENCE_TEMPLATE_PATH, filename)
     end
 
-    attr_reader :exclusion_pattern
-    attr_reader :directory
-    attr_reader :graph_type
-
     def initialize(argv, additional_options = {})
       @argv = argv
       @output = additional_options.fetch(:output, STDOUT)
 
       initialize_config_from_argv
       initialize_collaborators_from_configuration
-      initialize_attrs_from_configuration
     end
+
+    extend Forwardable
+    def_delegators :config, *[
+      :directory,
+      :graph_type,
+      :exclusion_pattern,
+    ]
 
     def copy_templates_into(directory)
       FileUtils.cp TEMPLATE_FILES, directory
@@ -36,7 +40,7 @@ class Turbulence
       FileUtils.mkdir_p("turbulence")
 
       Dir.chdir("turbulence") do
-        turb = Turbulence.new(directory, @output, @exclusion_pattern)
+        turb = Turbulence.new(directory, @output, exclusion_pattern)
 
         generator = case graph_type
         when "treemap"
@@ -104,12 +108,6 @@ class Turbulence
       Turbulence::Calculators::Churn.scm          = config.scm
       Turbulence::Calculators::Churn.commit_range = config.commit_range
       Turbulence::Calculators::Churn.compute_mean = config.compute_mean
-    end
-
-    def initialize_attrs_from_configuration
-      @exclusion_pattern = config.exclusion_pattern
-      @graph_type        = config.graph_type
-      @directory         = config.directory
     end
   end
 end
